116f4f458739aa4440cad5d74473240015c9a99d
harshit3610 <46259317+harshit3610@users.noreply.github.com>
2019-11-19 16:44:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodeReviewTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            Console.WriteLine("0,1")
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine("a+b");
+                a = b;
+                b = a;
+            }
+        }
+    }
+}
